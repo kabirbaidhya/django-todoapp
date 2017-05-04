6c22c538463038178ec1dbc55d970d09326da0d8
Kabir Baidhya <kabirbaidhya@gmail.com>
2017-05-05 04:56:13 +0545

    Add Todo model

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Todo(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField(null=True)
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField()
