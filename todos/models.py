1669e1224b654cc5a9cee26f75d13d1507297c89
Kabir Baidhya <kabirbaidhya@gmail.com>
2017-05-06 05:27:45 +0545

    Make the todo created_at default to current datetime with timezone

--- todos/models.py
+++ todos/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Todo(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField(null=True)
     completed = models.BooleanField(default=False)
-    created_at = models.DateTimeField()
+    created_at = models.DateTimeField(default=timezone.now)
