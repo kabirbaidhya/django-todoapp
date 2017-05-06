115ff9d962acee8477b23aff5682792855e3a63a
Kabir Baidhya <kabirbaidhya@gmail.com>
2017-05-06 10:00:58 +0545

    Add url & view function for edit page

--- todos/urls.py
+++ todos/urls.py
@@ -4,5 +4,6 @@ from todos import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^create$', views.create, name='create'),
-    url(r'^save$', views.save, name='save')
+    url(r'^save$', views.save, name='save'),
+    url(r'^edit/todos/(\d+)$', views.edit, name='edit'),
 ]
--- todos/views.py
+++ todos/views.py
@@ -29,3 +29,9 @@ def save(request):
 
     # Redirect back to index page
     return redirect('index')
+
+
+def edit(request, id):
+    print('Received Id = ' + str(id))
+
+    return render(request, 'create.html')
