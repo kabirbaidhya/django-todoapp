86c739c054a5c53f1875c5c21f9b8a4209297ff9
Kabir Baidhya <kabirbaidhya@gmail.com>
2017-05-06 10:40:02 +0545

    Add create & update logic in the save

--- todos/views.py
+++ todos/views.py
@@ -22,12 +22,31 @@ def save(request):
     title = request.POST.get('title')
     description = request.POST.get('description')
 
-    # Create a new todo item with the data.
-    Todo.objects.create(
-        title=title,
-        description=description,
-        created_at=timezone.now()
-    )
+    # Get hidden form data.
+    form_type = request.POST.get('form_type')
+    id = request.POST.get('id')
+
+    print('Form type received:', form_type)
+    print('Form todo id received:', id)
+
+    if form_type == 'create':
+        # Create a new todo item with the data.
+        todo = Todo.objects.create(
+            title=title,
+            description=description,
+            created_at=timezone.now()
+        )
+        print('New Todo created: ', todo.__dict__)
+    elif form_type == 'edit' and id.isdigit():
+        todo = Todo.objects.get(pk = id)
+        print('Got todo item: ', todo.__dict__)
+
+        # Save logic
+        todo.title = title
+        todo.description = description
+
+        todo.save()
+        print('Todo updated: ', todo.__dict__)
 
     # Redirect back to index page
     return redirect('index')
