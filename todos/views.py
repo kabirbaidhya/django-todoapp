f34a18a87564bdd2375c572f11678b58fa11a249
Kabir Baidhya <kabirbaidhya@gmail.com>
2017-05-07 06:32:45 +0545

    Add backend validation on save

--- todos/views.py
+++ todos/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.utils import timezone
+from django.contrib import messages
 
 from todos.models import Todo
 
@@ -29,6 +30,11 @@ def save(request):
     print('Form type received:', form_type)
     print('Form todo id received:', id)
 
+    # Validation logic
+    if title is None or title.strip() == '':
+        messages.error(request, 'Item not saved. Please provide the title.')
+        return redirect(request.META.get('HTTP_REFERER'))
+
     if form_type == 'create':
         # Create a new todo item with the data.
         todo = Todo.objects.create(
@@ -38,7 +44,7 @@ def save(request):
         )
         print('New Todo created: ', todo.__dict__)
     elif form_type == 'edit' and id.isdigit():
-        todo = Todo.objects.get(pk = id)
+        todo = Todo.objects.get(pk=id)
         print('Got todo item: ', todo.__dict__)
 
         # Save logic
@@ -56,7 +62,7 @@ def edit(request, id):
     print('Received Id: ' + str(id))
 
     # Fetch todo item by id
-    todo = Todo.objects.get(pk = id)
+    todo = Todo.objects.get(pk=id)
     print('Got todo item: ', todo.__dict__)
 
     return render(request, 'form.html', {
