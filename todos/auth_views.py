1bd995aa3aa81e6e8e454e6d0aaca39b7ae14368
Kabir Baidhya <kabirbaidhya@gmail.com>
2017-05-20 06:24:44 +0545

    Add authentication and login logic

--- todos/auth_views.py
+++ todos/auth_views.py
@@ -1,7 +1,25 @@
 from django.contrib import messages
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 
 def login(request):
     print('The login page')
 
     return render(request, 'login.html')
+
+def authenticate(request):
+    print('The authenticate request')
+    # Get the form data from the request.
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+
+    print('Authenticating the user')
+    user = authenticate(username=username, password=password)
+
+    # If authentication was successful
+    login(request, user)
+    print('Login successful')
+
+    # Add save success message
+    messages.info(request, 'You are now logged in as {}.'.format(username))
+    redirect('index')
