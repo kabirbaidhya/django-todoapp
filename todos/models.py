b331323f98abeeca6aa097fe5ce93d6a2cff24ff
Kabir Baidhya <kabirbaidhya@gmail.com>
2017-05-20 22:53:27 +0545

    Add an user attribute to the Todo model

--- todos/models.py
+++ todos/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.utils import timezone
-
+from django.contrib.auth.models import User
 
 class Todo(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField(null=True)
     completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(default=timezone.now)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
