92fe6a9ffd2f69f6fccbc6bceb478b3e176cf64c
Kabir Baidhya <kabirbaidhya@gmail.com>
2017-05-27 13:11:36 +0545

    Remove unnecessary old view code

--- todos/views_api.py
+++ todos/views_api.py
@@ -1,30 +1,6 @@
-import json
-from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from django.views.decorators.http import require_http_methods
 from rest_framework import generics
-
 from todos.models import Todo
 from todos.serializers import TodoSerializer
-
-
-@csrf_exempt
-@require_http_methods(['PATCH'])
-def update(request, id):
-    # Get the params from the payload.
-    data = json.loads(request.body.decode('utf-8'))
-
-    print('Received update API request for todo id: ', id)
-    print('Completed: ', data['completed'])
-
-    # Update the model
-    todo = Todo.objects.get(pk=id)
-    todo.completed = data['completed']
-    todo.save()
-
-    print('Todo item updated')
-
-    return JsonResponse({})
 
 
 class TodoListView(generics.ListCreateAPIView):
