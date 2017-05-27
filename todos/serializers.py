9430ad0008485d789478c6362b245a7829ea0de1
Kabir Baidhya <kabirbaidhya@gmail.com>
2017-05-27 12:42:50 +0545

    Add TodoSerializer

---
+++ todos/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import Todo
+
+
+class TodoSerializer(serializers.ModelSerializer):
+    ''' Serializer to map the Todo model to JSON. '''
+
+    class Meta:
+        """Meta class to map serializer's fields with the model fields."""
+        model = Todo
+        fields = ('id', 'title', 'description', 'completed', 'created_at')
+        read_only_fields = ('created_at',)
