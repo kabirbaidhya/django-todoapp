1a22f7215a1ced0d0fe63cff543feb6b99da423c
Kabir Baidhya <kabirbaidhya@gmail.com>
2017-05-29 05:24:20 +0545

    Setup django whitenoise config for static files

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -27,10 +27,6 @@ SECRET_KEY = 'gn8u7x-2z09t7dbn5%@5p5dj@3q1d^y9e1v54r1me=fwm^aw@f'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    '0.0.0.0'
-]
-
 
 # Application definition
 
@@ -127,5 +123,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+# STATIC_URL = '/static/'
+LOGIN_URL = 'login'
+
+ALLOWED_HOSTS = ['*']
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
-LOGIN_URL='login'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    # os.path.join(PROJECT_ROOT, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
